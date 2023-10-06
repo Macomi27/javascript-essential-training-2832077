e0219bc6f705b40c7e86bcc0335355b7979973ac
Macomi27 <macomfortitcham@gmail.com>
2023-10-06 04:24:44 +0100

    Challenge_ Build and modify an array

--- 07_04/script.js
+++ 07_04/script.js
@@ -8,4 +8,30 @@
  * - Remove the item you found using the find method from the array.
  */
 
+// 1
+Academics = ["book", "pen", "table", "chair", "eraser", "chalk", "blackboard", "pencil"]
+console.log(Academics);
 
+//2
+Academics.pop()
+console.log(Academics);
+
+//3
+Academics.unshift("pencil")
+console.log(Academics);
+
+//4
+Academics.sort()
+console.log(Academics);
+
+//5
+let Search = Academics.find(function (item) {
+    if (item.length >= 6) {
+        return item;
+    }
+})
+console.log("Academics item length greater than or equal to six: ", Search)
+
+//6
+Academics.shift(Search)
+console.log(Academics)
